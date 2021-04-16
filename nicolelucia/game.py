011635db997902452df93946357523ea285bbc4c
lucia-rehmann <50134819+lucia-rehmann@users.noreply.github.com>
2021-04-16 11:41:11 +0100

    Create game.py

---
+++ nicolelucia/game.py
@@ -0,0 +1,249 @@
+#i am trying to make a global thing to keep the score
+
+global score 
+score = 0
+#this is a function for a picture
+def house():
+  print("You are outside an abandoned house")
+
+  print("  ____||____.        ")
+  print(" ///////////\.       ")
+  print("///////////  \.      ")
+  print("|    _    |  |       ")
+  print("|[] | | []|[]|       ")
+  print("|   | |   |  |       ")
+#this is a function for a picture
+def tv():
+  print("     O        O      ")
+  print("     .\\     //      ")
+  print("     . \\   //       ")
+  print("     .  \\ //        ")
+  print("       /~~~~~\.      ")
+  print(",-------------------,")
+  print("| ,---------------, |")
+  print("| |               | |")
+  print("| |               | |")
+  print("| |               | |")
+  print("| |               | |")
+  print("| |_______________| |")
+  print("|___________________|")
+  print("|___________________|")
+#this is a function for a picture
+def shed():
+  print("          _          ") 
+  print("       _-'_'-_       ")    
+  print("    _-' _____ '-_    ")
+  print(" _-' ___________ '-_ ")
+  print("  |___|||||||||___|  ")
+  print("  |___|||||||||___|  ")
+  print("  |___|||||||o|___|  ")
+  print("  |___|||||||||___|  ")
+  print("  |___|||||||||___|  ")
+  print("  |___|||||||||___|  ")
+#this is a function for a picture
+def knife():
+  print("        / )          ")  
+  print("  |||| / /           ") 
+  print("  ||||/ /            ")                        
+  print("  \__(_/             ")                  
+  print("   ||//              ")                  
+  print("   ||/               ")                       
+  print("   ||                ")
+  print("  (||                ")
+#this is a function for a picture
+def doors():
+  print("  __ __   __ __      ")
+  print(" |  |  | |  |  |     ")
+  print(" | -|- | | -|- |     ")
+  print(" |__|__| |__|__|     ")
+#this is a function for the intro to my game
+def intro():
+  print("Welcome to my game, you will go around an abandoned house and try not to die. If you die you have to start again. The aim of the game is to get out of the abandoned house with at least 20 points. If you die, you have to start again. Have fun:)")
+
+
+
+
+from time import sleep
+
+#this is the function for the start of my game and a symbol
+def start():
+  pass
+  #i started a loop
+  
+  while True:
+    
+    
+    choice =input("type start to play ")
+    if choice =="start":
+      print("   _____     _______   ")
+      sleep(1)
+      print("  /      \  |      /\. ")
+      sleep(1)
+      print(" /_______/  |_____/  \.")
+      sleep(1)   
+      print("|   \   /        /   / ")
+      sleep(1)
+      print(" \   \         \/   /  ")
+      sleep(1)
+      print("  \  /          \__/_  ")
+      sleep(1)
+      print("   \/ ____    /\.      ")
+      sleep(1)
+      print("     /  \    /  \.     ")
+      sleep(1)
+      print("    /\   \  /   /      ")
+      sleep(1)
+      print("      \   \/   /       ")
+      sleep(1)
+      print("       \___\__/        ")
+      
+      break
+    
+
+    else:
+      print("start again")
+    
+
+def first():
+  global score
+  #i started a loop
+  while True :
+    choice = input("Do you want to go in the front door yes or no? ")
+
+    if choice =="yes":
+      print("you are going in the front door")
+      global score
+      score = score + 5
+      break
+      #i put break to stop the loop
+
+    else:
+      print("wrong answer")
+      score = score + 0
+      first()
+
+
+def doorsone():
+ global score
+  #i started a loop
+ while True:
+    print("when you go through the front door you see a hall with two doors")
+        
+    doors()
+
+    choice = input("do you want to go through door 1 or 2    ")
+
+    if choice =="1":
+      print("you die, start again")
+      score = score + 0
+      start()
+
+    if choice =="2" :
+      print("you are in the living room ")
+      score = score + 5
+      break
+      #i put break to stop the loop
+
+
+    else :
+      print("pick a door 1 or 2")
+      doorsone()
+
+def livingroom():
+  global score
+
+  #i started a loop
+  while True:
+    tv()
+
+    choice = input("pick one backgarden or TV ")
+
+    if choice =="backgarden":
+      print("you open the backdoor")
+      global score
+      score = score + 5
+      break
+      #i put break to stop the loop
+
+    if choice =="TV" or "tv" :
+      print("you switch on the TV and you die, start again")
+      global score
+      score = score + 0
+      start()
+    
+    else :
+      livingroom()
+
+def shedtwo():
+  #i started a loop
+ while True:
+   print("you can go to the grass or go into the shed")
+
+   shed()
+
+   choice = input("pick one, grass or shed ")
+
+   if choice =="shed":
+      print("you go into the shed and turn back around")
+      global score
+      score = score + 5
+      break
+      #i put break to stop the loop
+
+   if choice =="grass":
+    print("you get stung by a bee, start again")
+    global score
+    score = score + 0
+    start()
+
+   else :
+    shedtwo()
+
+def kitchen():
+#i started a loop
+  while True:
+    print("you can go upstairs or go to the kitchen ")
+
+    knife()
+
+    choice = input("are you going to go upstairs or to the kitchen? ")
+
+    if choice =="upstairs":
+        print("you go upstairs and see 4 doors")
+        global score
+        score = score + 5
+        break
+        #i put break to stop the loop
+    
+    doors()
+    doors()
+
+    if choice =="kitchen" :
+        print("you fell over and have to start again")
+        global score
+        score = score + 0
+        start()
+
+    else :
+      kitchen()
+
+def finalscore():
+  print("Your final score is..." , score)
+
+intro()
+
+start()
+
+first()
+
+house()
+
+doorsone()
+
+livingroom()
+
+shedtwo()
+
+kitchen()
+ 
+start()
