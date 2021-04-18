ac0db7ff961044d87735ab314753cb498bf144c4
Rapidery <45331049+Rapidery@users.noreply.github.com>
2021-04-18 16:10:38 +0100

    smoothing
    smoothed out some issues with spelling, and also added color and and code to make the normal "print" into something more interesting.

--- h.e.o./jailbreak.py
+++ h.e.o./jailbreak.py
@@ -1,56 +1,93 @@
+#random imports from libraries to make the code function properly
+from colorama import Fore
+import sys, time
 from time import sleep
 
+
+#the code for the jailcell part of the game
 def jail_cell():
 
   while True:
+    
+    for c in Fore.GREEN + "Escape from jail, You can only go one way, You cannot go back, So choose wisely, Succeed or you will die.":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+    print
 
-    print("escape from jail.")
-    sleep(2)
-    print("you can only go one way. you cannot go back.")
-    sleep(4)
-    print("so choose wisely.")
-    sleep(2)
-    print("succeed or you die.")
-    sleep(4)
-    print(" ")
-    jail_cell_ = input("You're at the door, what do your do? f(forward), b(you stay in jail.)")
+    print("   ")
+
+    jail_cell_ = input(Fore.RED + "You're at the door, what do you do? f(forward), b(you stay in jail.)")
 
     if jail_cell_ == "f":
-      print("you notice your cell dor is not locked properly. you pick it with hair clips you got for youtr inmate.")
+
+      for c in Fore.YELLOW + "you notice your cell door is not locked properly. you pick it with hair clips you got for your fellow inmate.":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+
       corridor()
       break
 
     elif jail_cell_ == "b":
-      print("you stay behind bars. unable to do anything or see anyone.")
+      print(Fore.YELLOW + "you stay behind bars. unable to do anything or see anyone.")
       gameover()
       break
 
     else:
-      print("you cant go there.")
+      print(Fore.YELLOW + "you can't go there.")
 
+sleep(2)
+
+#the code for the gameover section of "jailcell"
 def gameover():
-  print("you die of depression.")
+  print(Fore.YELLOW + "you die of depression in a jail where your soul will be trapped till the end of this universe.")
 
+sleep(2)
 
+#the code for section two, the corridot of left and right
 def corridor():
 
   while True:
+    
+    print("   ")
 
-    choice1 = input("You're in the corridor, what do your do? r(go right), l(go left)")
+    for c in Fore.RED + "You're in the corridor, what do your do? r(go right), l(go left)":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+    print
+
+    choice1 = input("  ")
 
     if choice1 == "r":
-      print("you walk right. you see the cafiteria in the distance")
+     
+      for c in Fore.YELLOW + "you walk right. you see the cafeteria in the distance":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+
       #cafeteria()
       break
 
 
     elif choice1 == "l":
-      print("you walk left. you arrive at the gym")
+      for c in Fore.YELLOW + "you walk right, you found the gym":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
       #gym()
       break
 
 
     else:
-      print("you cannot go there")
+      for c in Fore.YELLOW + "Not an option buddy, get lost":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
 
 jail_cell()
