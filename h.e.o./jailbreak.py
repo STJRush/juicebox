ac964ee7952a27c08acc3fa7111aa591dfd520c7
Rapidery <45331049+Rapidery@users.noreply.github.com>
2021-04-22 11:55:46 +0100

    Update jailbreak.py

--- h.e.o./jailbreak.py
+++ h.e.o./jailbreak.py
@@ -9,18 +9,21 @@ def jail_cell():
 
   while True:
     
+    #just some random backstory
     for c in Fore.GREEN + "Escape from jail, You can only go one way, You cannot go back, So choose wisely, Succeed or you will die.":
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.07)
     print
-
     print("   ")
 
-    jail_cell_ = input(Fore.RED + "You're at the door, what do you do? f(forward), b(you stay in jail.)")
+    #code to decide if you leave the cell or not
+    jail_cell_ = input(Fore.RED + "You're at the door, what do you do? n(go north), s(you stay in jail.)")
 
-    if jail_cell_ == "f":
-
+    #if you decide to leave this will run
+    if jail_cell_ == "n":
+      
+      #this is just some random dialouge lol
       for c in Fore.YELLOW + "you notice your cell door is not locked properly. you pick it with hair clips you got for your fellow inmate.":
         sys.stdout.write(c)
         sys.stdout.flush()
@@ -30,11 +33,13 @@ def jail_cell():
       corridor()
       break
 
-    elif jail_cell_ == "b":
+    #this code will run if you decide to stay in the cell resulting in a restart of the game
+    elif jail_cell_ == "s":
       print(Fore.YELLOW + "you stay behind bars. unable to do anything or see anyone.")
       gameover()
       break
 
+    #if the player types anything but the correct letter this code will run
     else:
       print(Fore.YELLOW + "you can't go there.")
 
@@ -53,7 +58,8 @@ def corridor():
     
     print("   ")
 
-    for c in Fore.RED + "You're in the corridor, what do your do? r(go right), l(go left)":
+    #when you leave the cell you are given two options to walk, its up to you xD
+    for c in Fore.RED + "You're in the corridor, what do your do? n(go north), w(go west)":
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.07)
@@ -61,8 +67,10 @@ def corridor():
 
     choice1 = input("  ")
 
-    if choice1 == "r":
+    #here it says that if you choose east it will run this code
+    if choice1 == "n":
      
+      #this code just says if you chose east you will see a cafeteria
       for c in Fore.YELLOW + "you walk right. you see the cafeteria in the distance":
         sys.stdout.write(c)
         sys.stdout.flush()
@@ -73,8 +81,167 @@ def corridor():
       break
 
 
-    elif choice1 == "l":
+    #if you chose to walk west this code will run
+    elif choice1 == "w":
+
+      #this code says if you chose west you will find a gym
       for c in Fore.YELLOW + "you walk right, you found the gym":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+      gym()
+      break
+
+    #this code is to prevent people from typing anything not allowed
+    else:
+      for c in Fore.YELLOW + "Not an option buddy, get lost":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+
+#this is the code for the cafeteria section
+def cafeteria():
+
+  while True:
+
+    print("   ")
+
+    #this code gives you two options to walk
+    for c in Fore.RED + "You're at the cafeteria, what do your do? r(go right), l(go left)":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+    print
+
+    choice1 = input("  ")
+
+    #this code says if you chose right _______ will happen
+    if choice1 == "r":
+      
+      #ignore this for now please as this is incomplete___________________________
+      for c in Fore.YELLOW + "random room here":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+
+      #cafeteria()
+      break
+
+
+    #this code is if you chose to go left
+    elif choice1 == "l":
+
+      #it says another room as this is incomplete, ignore this _______________________________________
+      for c in Fore.YELLOW + "another room":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+
+      #gym()
+      break
+
+
+    #once again this code is for those who try to be rebels, VIVA LA REVOLUTION
+    else:
+      for c in Fore.YELLOW + "Not an option buddy, get lost":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+
+#this is the code for the gym section
+def gym():
+
+  while True:
+
+    print("   ")
+
+    #this gives you two options to walk while in the gym
+    for c in Fore.RED + "You're at the gym, what do your do? n(go north), e(go east)":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+    print
+
+    choice1 = input("  ")
+
+    #if you chose to go north this code will run
+    if choice1 == "n":
+     
+      #listen here buddy we aint finished alright?________________________________________
+      for c in Fore.YELLOW + "random room here":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+
+      library()
+      break
+
+    #if you chose east this will run
+    elif choice1 == "e":
+
+      #you ancounter :O, another room that isnt finished, my oh my you could have forseen this :)
+      for c in Fore.YELLOW + "another room":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+
+      print
+      corridor()
+      break
+
+
+    #gotta stop them rebels so this code makes it so the game dosnt die because of a crash xD
+    else:
+      for c in Fore.YELLOW + "Not an option buddy, get lost":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+
+#this is the code for the gym section
+def library():
+
+  while True:
+
+    print("   ")
+
+    for c in Fore.RED + "You're at the library, what do your do? e(go east), ne(go north east), s(go south)":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+    print
+
+    choice1 = input("  ")
+
+    if choice1 == "e":
+     
+      for c in Fore.YELLOW + "random room here":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+
+      yard()
+      break
+
+
+    elif choice1 == "ne":
+      for c in Fore.YELLOW + "another room":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+      hospital()
+      break
+
+    elif choice1 == "s":
+      for c in Fore.YELLOW + "another room":
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.07)
@@ -90,14 +257,13 @@ def corridor():
         time.sleep(0.07)
       print
 
-
-def cafeteria():
+def yard():
 
   while True:
 
     print("   ")
 
-    for c in Fore.RED + "You're at the cafeteria, what do your do? r(go right), l(go left)":
+    for c in Fore.RED + "You're in the yard, what do your do? n(go north), s(go south), w(go west), e(go east)":
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.07)
@@ -105,7 +271,7 @@ def cafeteria():
 
     choice1 = input("  ")
 
-    if choice1 == "r":
+    if choice1 == "w":
      
       for c in Fore.YELLOW + "random room here":
         sys.stdout.write(c)
@@ -113,19 +279,36 @@ def cafeteria():
         time.sleep(0.07)
       print
 
-      #cafeteria()
+      library()
       break
 
 
-    elif choice1 == "l":
+    elif choice1 == "s":
       for c in Fore.YELLOW + "another room":
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.07)
       print
-      #gym()
+      cafeteria()
       break
 
+    elif choice1 == "e":
+      for c in Fore.YELLOW + "another room":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+      #meeting_room()
+      break
+
+    elif choice1 == "n":
+      for c in Fore.YELLOW + "another room":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+      hospital()
+      break
 
     else:
       for c in Fore.YELLOW + "Not an option buddy, get lost":
@@ -133,14 +316,14 @@ def cafeteria():
         sys.stdout.flush()
         time.sleep(0.07)
       print
-      
-def gym():
+
+def hospital():
 
   while True:
 
     print("   ")
 
-    for c in Fore.RED + "You're at the gym, what do your do? r(go right), l(go left)":
+    for c in Fore.RED + "You're at the hospital, what do your do? w(go west), s(go south) e(go east)":
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.07)
@@ -148,7 +331,7 @@ def gym():
 
     choice1 = input("  ")
 
-    if choice1 == "r":
+    if choice1 == "w":
      
       for c in Fore.YELLOW + "random room here":
         sys.stdout.write(c)
@@ -156,19 +339,27 @@ def gym():
         time.sleep(0.07)
       print
 
-      #cafeteria()
+      library()
       break
 
 
-    elif choice1 == "l":
+    elif choice1 == "s":
       for c in Fore.YELLOW + "another room":
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.07)
       print
-      #gym()
+      yard()
       break
 
+    elif choice1 == "e":
+      for c in Fore.YELLOW + "another room":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.07)
+      print
+      #lobby()
+      break
 
     else:
       for c in Fore.YELLOW + "Not an option buddy, get lost":
@@ -176,5 +367,4 @@ def gym():
         sys.stdout.flush()
         time.sleep(0.07)
       print
-      
 jail_cell()
