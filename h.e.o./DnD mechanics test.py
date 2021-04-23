da11153bd40a4b34afddadbe096e1ff1fc8b84bd
Rapidery <45331049+Rapidery@users.noreply.github.com>
2021-04-23 11:48:02 +0100

    Update DnD mechanics test.py

--- h.e.o./DnD mechanics test.py
+++ h.e.o./DnD mechanics test.py
@@ -1,32 +1,71 @@
+#just the random libraries for this code
 import random
 import sys, time
 from time import sleep
 
-random_number = random.randint(1, 10)
-random_minimum = random.randint(2, 9)
+#the small line which chooses a random number between 1 and 10 to decide if you get an encounter
+EnemyEncounter = random.randint(1, 10)
 
 
-for c in "This enemies health is ":
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-print
+#the code for the actual fight
+def fight(): 
+  random_number = random.randint(1, 10)
+  random_minimum = random.randint(2, 9)
 
-print(random_minimum)
+  for c in "This enemies health is ":
+      sys.stdout.write(c)
+      sys.stdout.flush()
+      time.sleep(0.07)
+  print
 
-sleep(2)
+  print(random_minimum)
 
-for c in "rolling your damage....... you hit the enemy for ":
-    sys.stdout.write(c)
-    sys.stdout.flush()
-    time.sleep(0.07)
-print
+  sleep(2)
 
-print(random_number)
+  for c in "rolling your damage....... you hit the enemy for ":
+      sys.stdout.write(c)
+      sys.stdout.flush()
+      time.sleep(0.07)
+  print
+
+  print(random_number)
+
+  
+  if random_number >= random_minimum:
+    print("You hit the enemy with enough force to eliminate him")
+  else:
+    print("you were to weak, go back to training")
 
 
-if random_number >= random_minimum:
-  print("You hit the enemy with enough force to eliminate him")
+#the code if you failed the chance encounter
+def Nencounter():
+ 
+  for c in "You did not encounter an enemy":
+      sys.stdout.write(c)
+      sys.stdout.flush()
+      time.sleep(0.07)
+  print
+
+
+#the code if you succeded the chance enounter
+def Yencounter():
+
+  
+  for c in "You encountered an [Enemy] ":
+      sys.stdout.write(c)
+      sys.stdout.flush()
+      time.sleep(0.07)
+  print
+
+  sleep(1)
+
+  fight()
+
+  
+
+#just the code to decide if you get an encounter or not
+if EnemyEncounter < 2:
+  Yencounter()
 else:
-  print("you were to weak, go back to training")
-
+  Nencounter()
+ 
