ce67ea9682b7936740359c4baa137abdb3cdb19b
Rapidery <45331049+Rapidery@users.noreply.github.com>
2021-04-26 10:11:42 +0100

    Update DnD mechanics test.py

--- h.e.o./DnD mechanics test.py
+++ h.e.o./DnD mechanics test.py
@@ -12,25 +12,29 @@ def fight():
   random_number = random.randint(1, 10)
   random_minimum = random.randint(2, 9)
 
+  #this is just the code to make the text appear as if it was being typed
   for c in "This enemies health is ":
       sys.stdout.write(c)
       sys.stdout.flush()
       time.sleep(0.07)
   print
 
+  #this was originally a debug code but now it tells the user the health of the enemy
   print(random_minimum)
 
   sleep(2)
-
+  
+  #the same as last time this just makes it appear as if someone was typing
   for c in "rolling your damage....... you hit the enemy for ":
       sys.stdout.write(c)
       sys.stdout.flush()
       time.sleep(0.07)
   print
-
+  
+  #this was debug code but now it tells the user how much damage the player did
   print(random_number)
 
-  
+  #this is the code to decide if you did enough damage to eliminate the enemy
   if random_number >= random_minimum:
     print("You hit the enemy with enough force to eliminate him")
   else:
@@ -50,7 +54,7 @@ def Nencounter():
 #the code if you succeded the chance enounter
 def Yencounter():
 
-  
+  #code to appear as if you were typing
   for c in "You encountered an [Enemy] ":
       sys.stdout.write(c)
       sys.stdout.flush()
@@ -58,7 +62,8 @@ def Yencounter():
   print
 
   sleep(1)
-
+  
+  #if this def is chosen it started the fight def
   fight()
 
   
